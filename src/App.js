19632813922c54296f999c63272c2f9fe66aeb1f
adrian <adrian@pinchaaqui.es>
2022-03-30 19:37:57 +0200

    ejercicio 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+
+  const [categories, setCategories] =useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    all: 0
+  });
+
+  const handleGood = () => {
+    setCategories({
+      ...categories,
+      good: categories.good + 1,
+      all: categories.all +1
+    })
+
+  }
+
+  const handleNeutral= () => {
+    setCategories({
+      ...categories,
+      neutral: categories.neutral + 1,
+      all: categories.all +1
+    })
+  }
+
+  const handleBad = () => {
+    setCategories({
+      ...categories,
+      bad: categories.bad + 1,
+      all: categories.all +1
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+
+      <h2>statics</h2>
+
+    <p>good {categories.good}</p>  
+    <p>neutral {categories.neutral}</p>  
+    <p>bad {categories.bad}</p>  
+    <p>all {categories.all}</p> 
+
+
     </div>
   );
 }
